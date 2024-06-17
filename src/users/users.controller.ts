3b837d37fae0ee77efa50119a4f1ca0128c75590
Dima <dimashikalov86@mail.ru>
2024-06-17 17:25:25 +0400

    create first decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,10 +3,15 @@ import { BaseController } from '../common/base.controller';
 import { IControllerRoute } from '../common/route.interface';
 import { LoggerService } from '../logger/logger.service';
 import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
 
+@injectable()
 export class UserController extends BaseController{
-	constructor( logger: LoggerService) {
-		super(logger)
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService)
 		
 		this.bindRoutes([
 			{path:'/register', method: 'post', func: this.register},

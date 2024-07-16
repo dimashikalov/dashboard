88fbd296e3b8fa91a42a7738711e9b038ce55174
Dima <dimashikalov86@mail.ru>
2024-07-16 18:19:36 +0300

    added user.entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import 'reflect-metadata';
 import { IUserController } from './users.controlle.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -27,8 +28,14 @@ export class UserController extends BaseController implements IUserController {
 
 		next(new HTTPError(401, 'No login', 'login'));
 	}
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
-		this.ok(res, 'register');
+
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok(res, newUser);
 	}
 }
